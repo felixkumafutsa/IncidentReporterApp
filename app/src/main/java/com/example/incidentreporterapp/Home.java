0643d2906e4eaf3ff151765156addd1166e5d9e4
Incident Reporter <felixkumafutsa@gmail.com>
2022-02-11 02:17:18 +0200

    project code updated

--- app/src/main/java/com/example/incidentreporterapp/Home.java
+++ app/src/main/java/com/example/incidentreporterapp/Home.java
@@ -1,23 +1,11 @@
 package com.example.incidentreporterapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
     Button panicButton, quickCall, captureIncident, login;
--- app/src/main/java/com/example/incidentreporterapp/QuickMsg.java
+++ app/src/main/java/com/example/incidentreporterapp/QuickMsg.java
@@ -1,46 +1,30 @@
 package com.example.incidentreporterapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.location.Address;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.telephony.SmsManager;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.incidentreporterapp.databinding.ActivityIncidentsMapBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class QuickMsg extends AppCompatActivity {
     private Spinner department;
@@ -67,23 +51,10 @@ public class QuickMsg extends AppCompatActivity {
         message = "Help me!, it's an emergency\n please reach ASAP to the location below\n   ";
         department = findViewById(R.id.departmentSpinner);
            sendPanicSituation = findViewById(R.id.sendPanic);
-       /* String  []  = {"FIRE", "MASM","POLICE"};
-        String firePhone = "0997473256";
-        String masmPhone = "0991764917";
-        String policePhone = "0991764917";*/
-
-        HashMap<String, String> departments = new HashMap<String, String>();
-
-        // Add keys and values (Country, City)
-        departments.put("FIRE", "0997473256");
-        departments.put("MASM", "0991764917");
-        departments.put("POLICE", "0991764917");
-
-
 
         Spinner spinnerDepartment = (Spinner) findViewById(R.id.departmentSpinner);
         ArrayAdapter<CharSequence> adapterDepartment = ArrayAdapter.createFromResource(this,
-                R.array.departments_array, android.R.layout.simple_spinner_item);
+                R.array.phoneNumber_array, android.R.layout.simple_spinner_item);
         adapterDepartment.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDepartment.setAdapter(adapterDepartment);
 
